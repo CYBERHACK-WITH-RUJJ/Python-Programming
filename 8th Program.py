0c54910e773fba80430120a31f6fb90db402a9c4
RujviWeginwar <rweginwar@gmail.com>
2024-07-14 23:30:59 +0530

    added a new code

---
+++ 8th Program.py
@@ -0,0 +1,10 @@
+P = float(input('Enter principal amount: '))
+R = float(input('Enter the interest rate: '))
+T = float(input('Enter time: '))
+
+# calculate simple interest
+SI = (P * R * T) / 100
+
+# display result
+print('Simple interest = ',SI )
+print('Total amount = ',( P + SI ))
